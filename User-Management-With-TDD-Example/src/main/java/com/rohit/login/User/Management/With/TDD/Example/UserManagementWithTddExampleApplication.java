d822064f90d347828863f898676aa8610f523e4a
Rohit Chavan <53335793+rohitchavan-git@users.noreply.github.com>
2022-06-08 11:26:35 +0530

    init project structure

---
+++ User-Management-With-TDD-Example/src/main/java/com/rohit/login/User/Management/With/TDD/Example/UserManagementWithTddExampleApplication.java
@@ -0,0 +1,13 @@
+package com.rohit.login.User.Management.With.TDD.Example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UserManagementWithTddExampleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UserManagementWithTddExampleApplication.class, args);
+	}
+
+}
---
+++ User-Management-With-TDD-Example/src/test/java/com/rohit/login/User/Management/With/TDD/Example/UserManagementWithTddExampleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rohit.login.User.Management.With.TDD.Example;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UserManagementWithTddExampleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
